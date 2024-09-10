83dcdbc17a0423cede913367439ecd3cb84dc491
marcelot <marcelojtelles@gmail.com>
2024-09-10 10:26:17 -0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,16 @@
+#Comandos básicos do projeto
+
+##criar anbiente
+python3 -m venv venv
+
+##ativar ambiente Linux MacOs
+source ./venv/bin/activate
+
+##ativar ambiente Windows
+myenv\Scripts\activate
+
+##instalar dependências
+pip3 install -r requirements.txt 
+
+##rodar
+python3 run.py 
---
+++ app/routes.py
@@ -0,0 +1,36 @@
+from flask import request, redirect, url_for, render_template
+from app import app, db
+from app.models import Item
+
+@app.route('/')
+def index():
+    items_ = Item.query.all()
+    return render_template('index.html', items=items_)
+
+@app.route('/create', methods=['GET', 'POST'])
+def create():
+    if request.method == 'POST':
+        name = request.form['name']
+        description = request.form['description']
+        new_item = Item(name=name, description=description)
+        db.session.add(new_item)
+        db.session.commit()
+        return redirect(url_for('index'))
+    return render_template('create.html')
+
+@app.route('/update/<int:id>', methods=['GET', 'POST'])
+def update(id):
+    item = Item.query.get_or_404(id)
+    if request.method == 'POST':
+        item.name = request.form['name']
+        item.description = request.form['description']
+        db.session.commit()
+        return redirect(url_for('index'))
+    return render_template('update.html', item=item)
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    item = Item.query.get_or_404(id)
+    db.session.delete(item)
+    db.session.commit()
+    return redirect(url_for('index'))
